412081f4a3c845f35cddbbd7a6f9d3b3e3ba1f6c
johnsamsa <49206310+johnsamsa@users.noreply.github.com>
2020-07-22 19:56:33 +0100

    22/07 - Implementação da stylesheet e do jQuery e arranjo da navbar

--- header.php
+++ header.php
@@ -4,25 +4,59 @@
     <meta charset="iso-8859-1">
     <meta name="viewport" content="width = device-width, initial-scale=1">
     <title> Merlin </title>
-    <link rel="stylesheet" href="style.css"
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <script src="js/bootstrap.js"></script>
+    <script src="js/jquery.js"></script>
   </head>
 
   <body>
 
     <header>
-
-      <nav>
-        <a href="#">
-          <img src="img/logo.png">
+      <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+        <a class="navbar-brand" href="#">
+          <img src="img/logo.png" width="30" height="30" alt="" loading="lazy">
         </a>
-        <ul>
-          <li><a href="#">Início</a></li>
-          <li><a href="#">Livros</a></li>
-          <li><a href="cadlivros.php">Cadastrar Livro</a></li>
-          <li><a href="#">Sair</a></li>
-        </ul>
-      </nav>
+        <span class="navbar-text" id="navbarSupportedContent">Merlin</span>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="true" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
 
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul class="navbar-nav mr-auto">
+            <li class="nav-item active">
+              <a class="nav-link" href="index.php">Merlin<span class="sr-only">(current)</span></a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="cadlivros.php">Cadastrar Livro</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#" tabindex="-1">Romances</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#" tabindex="-1">Biografias</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#" tabindex="-1">Terror</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#" tabindex="-1">Fantasia</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#" tabindex="-1">Livros Técnicos</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#" tabindex="-1">Livros de Poesia</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#" tabindex="-1">Ficção Científica</a>
+            </li>
+          </ul>
+          <form class="form-inline my-2 my-lg-0">
+            <input class="form-control mr-sm-2" type="search" placeholder="Pesquisar Livro" aria-label="Search">
+            <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Pesquisar</button>
+          </form>
+        </div>
+</nav>
     </header>
 
   </body>
